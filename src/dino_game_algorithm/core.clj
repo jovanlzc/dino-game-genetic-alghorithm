0da15561830811f627c082e3641917ac9ea3f92a
jovanl <jovan.lazic@plusplus.rs>
2020-09-01 15:37:37 +0200

    unapredjena cross-over funkcija

--- src/dino_game_algorithm/core.clj
+++ src/dino_game_algorithm/core.clj
@@ -11,23 +11,23 @@
 
 (defn select [chromosomes](take 2 chromosomes))
 
-(defn swap-elements [offset1 offset2] 
-  ())
 
-(defn change-chromosome [chromosomes new-offset]
-  ())
+(defn change-chromosome [chromosomes new-offset1 new-offset2]
+  (conj (drop-last 2 chromosomes) new-offset1 new-offset2))
 
 (defn cross-over [chromosomes]
   (let [[offset1 offset2 &rest] chromosomes]
     (let [cross-over-point (int (Math/floor (* (rand) (count offset1))))]
-     (loop [remaining-offsets1 (take cross-over-point offset1) remaining-offsets2 (take cross-over-point offset2) new-offset1 [] new-offset2 []]
+     (loop [remaining-offsets1 offset1 remaining-offsets2 offset2 new-offset1 [] new-offset2 []]
        (if (or (empty? remaining-offsets1) (empty? remaining-offsets2))
          (change-chromosome chromosomes new-offset1 new-offset2)
          (if (< (count new-offset1) cross-over-point)
-           (let [swapped-elements (swap-elements (take 1 remaining-offsets1)  (take 1 remaining-offsets2))] 
-             (recur (drop 1 remaining-offsets1) (drop 1 remaining-offsets2) (conj new-offset1 (first swapped-elements)) (conj new-offset2 (second swapped-elements))) )
-           (recur (drop 1 remaining-offsets1) (drop 1 remaining-offsets2) (conj new-offset1 (take 1 remaining-offsets1) (conj new-offset2 (take 2 remaining-offsets2))))
-           ))))))
+           (recur (drop 1 remaining-offsets1) (drop 1 remaining-offsets2) (conj new-offset1 (first remaining-offsets2)) (conj new-offset2 (first remaining-offsets1)))
+           (recur (drop 1 remaining-offsets1) (drop 1 remaining-offsets2) (conj new-offset1 (first remaining-offsets1)) (conj new-offset2 (first remaining-offsets2)))
+           )))
+         
+         )
+  ))
 
 (defroutes my_routes
   (GET "/" [] (view/index-page))
